3c60c76b666f1ec9907479a41108292cd9ac8cb7
peterheinum <peter.heinum@gmail.com>
2020-04-11 15:33:55 +0200

    Added Koa for letting bot be awake at the right times

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require('dotenv').config()
 const Discord = require('discord.io')
-const auth = process.env.DISCORDTOKEN
+
+/* Koa server for waking up the bot */
+const Koa = require('koa')
+const app = new Koa()
+const PORT = process.env.port || 3000 
+
+app.use(ctx => {
+  ctx.body = 'https://www.youtube.com/watch?v=I_izvAbhExY'
+})
+ 
+app.listen(PORT)
+console.log(`Koa open on ${PORT}`)
 
 // Initialize Discord Bot
 const bot = new Discord.Client({
-  token: auth,
+  token: process.env.DISCORDTOKEN,
   autorun: true
 })
 
@@ -13,6 +24,7 @@ const toArray = obj => keys(obj).map(key => obj[key])
 
 /* THIS BOT IS MEANT TO REPLACE CHRIS */
 bot.on('ready', event => {
+  console.log('Connected to Discord')
   const { servers } = bot
   const boiz = toArray(servers).find(({ name }) => name === 'Boiz')
   const boizId = boiz.id
@@ -20,7 +32,6 @@ bot.on('ready', event => {
   const chatRoom = toArray(bot.channels).find(x => x.guild_id === boizId)
 
   const to = chatRoom['id']
-  let msgSent = false
   setInterval(() => {
     const now = new Date()
     const [hours, minutes] = [now.getHours(), now.getMinutes()]
@@ -28,11 +39,7 @@ bot.on('ready', event => {
   
     if(message === '13:37' && !msgSent) {
       bot.sendMessage({ to, message })
-      msgSent = true
     }
 
-    msgSent = false
-  }, 30000)
-
+  }, 55000)
 })
-
